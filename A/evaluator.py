c84445a77ff8d4c341db659f3b135cfa024e8b29
AkshatSood <soodakshat30@gmail.com>
2024-05-05 13:12:43 +0100

    Update evaluator script

---
+++ A/evaluator.py
@@ -0,0 +1,80 @@
+import os
+import json, codecs
+import torch
+import pandas as pd
+from tqdm import tqdm
+from sklearn.metrics import (
+    accuracy_score,
+    f1_score,
+    precision_score,
+    recall_score,
+    confusion_matrix,
+)
+
+
+class Evaluator:
+
+    def __init__(self, x_test, y_test, label_map: dict, output_dir):
+        self.x_test = x_test
+        self.y_test = y_test
+        self.label_map = label_map
+        self.output_dir = output_dir
+
+    def create_evaluations(self, model, tokenizer, name):
+
+        classes = [self.label_map[i] for i in self.y_test]
+        outputs_df = pd.DataFrame(
+            data={"text": self.x_test, "test_class": classes, "test_label": self.y_test}
+        )
+
+        pred_labels = []
+        pred_classes = []
+        for text in tqdm(self.x_test):
+            inputs = tokenizer.encode(text, return_tensors="pt")
+            logits = model(inputs).logits
+            predictions = torch.argmax(logits)
+
+            label = predictions.tolist()
+
+            pred_labels.append(label)
+            pred_classes.append(self.label_map[label])
+
+        outputs_df["pred_classes"] = pred_classes
+        outputs_df["pred_labels"] = pred_labels
+
+        evaluation = {
+            "model": name,
+            "accuracy": accuracy_score(y_true=self.y_test, y_pred=pred_labels),
+            "f1_score": f1_score(
+                y_true=self.y_test, y_pred=pred_labels, average="macro"
+            ),
+            "precision": precision_score(
+                y_true=self.y_test, y_pred=pred_labels, average="macro"
+            ),
+            "recall": recall_score(
+                y_true=self.y_test, y_pred=pred_labels, average="macro"
+            ),
+            "confusion_matrix": confusion_matrix(
+                y_true=self.y_test,
+                y_pred=pred_labels,
+                labels=list(self.label_map.keys()),
+            ).tolist(),
+        }
+
+        print(evaluation)
+
+        # Write the outputs to a CSV file
+        outputs_df.to_csv(os.path.join(self.output_dir, f"{name}-outputs.csv"))
+
+        # Write the evaluations to a JSON file
+        json.dump(
+            evaluation,
+            codecs.open(
+                os.path.join(self.output_dir, f"{name}-evaluation.json"),
+                "w",
+                encoding="utf-8",
+                separators=(",", ":"),
+                sort_keys=True,
+                indent=4,
+            ),
+        )
