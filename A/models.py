c436461190d9cb6659e12193d6dfdff428d2256c
AkshatSood <soodakshat30@gmail.com>
2024-05-05 01:02:18 +0100

    Add model code for bert-base-uncased

--- A/models.py
+++ A/models.py
@@ -1,17 +1,36 @@
 from transformers import (
-    BertTokenizer,
-    BertForSequenceClassification,
     AutoModelForSequenceClassification,
     AutoTokenizer,
 )
 
 
 class BertBaseUncased:
-    def __init__(self):
-        tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
-        model = BertForSequenceClassification.from_pretrained(
-            "bert-base-uncased", num_labels=4
+    def __init__(
+        self,
+        num_labels: int = 2,
+        id2label: dict = None,
+        label2id: dict = None,
+        path=None,
+    ):
+        self.name = "bert-base-uncased"
+
+        self.model = AutoModelForSequenceClassification.from_pretrained(
+            self.name if path is None else path,
+            num_labels=num_labels,
+            id2label=id2label,
+            label2id=label2id,
         )
+
+        self.tokenizer = AutoTokenizer.from_pretrained(self.name, add_prefix_space=True)
+
+        self.tokenizer.truncation_side = "left"
+
+        if self.tokenizer.pad_token is None:
+            self.tokenizer.add_special_tokens({"pad_token": "[PAD]"})
+            self.model.resize_token_embeddings(len(self.tokenizer))
+
+    def load(self):
+        return self.model, self.tokenizer
 
 
 class DistilBertUncased:
