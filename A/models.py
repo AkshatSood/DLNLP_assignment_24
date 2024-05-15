1a117ce3851b0859fad84683c67f5fbc47b08c9e
AkshatSood <soodakshat30@gmail.com>
2024-05-15 12:36:46 +0100

    Create NaiveBayes classifier model

--- A/models.py
+++ A/models.py
@@ -1,3 +1,7 @@
+from sklearn.pipeline import make_pipeline, Pipeline
+from sklearn.model_selection import GridSearchCV
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.feature_extraction.text import TfidfVectorizer
 from transformers import (
     DistilBertForSequenceClassification,
     BertForSequenceClassification,
@@ -6,6 +10,61 @@ from transformers import (
     DistilBertTokenizerFast,
     RobertaTokenizerFast,
 )
+
+
+class NaiveBayes:
+
+    def __init__(
+        self,
+        vect_max_features: int = 20000,
+        vect_ngram_range: tuple = (1, 1),
+        vect_use_idf: bool = True,
+        clf_alpha: float = 0.1,
+        gridsearch: bool = False,
+        gs_vect_max_features: list = [5000, 10000, 15000, 20000],
+        gs_vect_ngram_range: list = [(1, 1), (1, 2), (2, 2)],
+        gs_vect_use_idf: list = [True, False],
+        gs_clf_alpha: list = [0.1, 0.5, 1.0],
+        gs_cv: int = 5,
+        gs_verbose: int = 5,
+    ):
+
+        self.gridsearch = gridsearch
+        if gridsearch:
+            pipeline = Pipeline([("vect", TfidfVectorizer()), ("clf", MultinomialNB())])
+
+            parameters = {
+                "vect__max_features": gs_vect_max_features,
+                "vect__ngram_range": gs_vect_ngram_range,
+                "vect__use_idf": gs_vect_use_idf,
+                "clf__alpha": gs_clf_alpha,
+            }
+
+            self.model = GridSearchCV(
+                pipeline, parameters, cv=gs_cv, verbose=gs_verbose, n_jobs=-1
+            )
+
+        else:
+            self.model = make_pipeline(
+                TfidfVectorizer(
+                    max_features=vect_max_features,
+                    ngram_range=vect_ngram_range,
+                    use_idf=vect_use_idf,
+                ),
+                MultinomialNB(alpha=clf_alpha),
+            )
+
+    def fit(self, x_train: list, y_train: list) -> None:
+        if self.gridsearch:
+            print("\n=>Performing Grid Search Cross Validation\n")
+
+        self.model.fit(x_train, y_train)
+
+        if self.gridsearch:
+            print(f"\n=> Best Parameters from Grid Search: {self.model.best_params_}")
+
+    def predict(self, x: list) -> list:
+        return self.model.predict(x)
 
 
 class BertBaseUncased:
