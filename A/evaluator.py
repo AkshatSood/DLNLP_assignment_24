0b74bd0b9cadcd912e67b994b78182cfe25d9c54
AkshatSood <soodakshat30@gmail.com>
2024-05-16 20:51:55 +0100

    Update evaluator.py

--- A/evaluator.py
+++ A/evaluator.py
@@ -96,18 +96,17 @@ class Evaluator:
     def create_nb_evaluations(
         self,
         model,
-        x_train: list,
-        y_train: list,
-        x_val: list,
-        y_val: list,
         x_test: list,
         y_test: list,
         model_name: str,
         task_name: str,
+        x_train: list = None,
+        y_train: list = None,
+        x_val: list = None,
+        y_val: list = None,
+        only_test: bool = False,
     ) -> list:
 
-        y_train_pred = model.predict(x_train)
-        y_val_pred = model.predict(x_val)
         y_test_pred = model.predict(x_test)
 
         test_evaluation = {
@@ -127,69 +126,75 @@ class Evaluator:
             ).tolist(),
         }
 
-        all_evaluations = [
-            {
-                "model": model_name,
-                "task": task_name,
-                "split": "test",
-                "evaluation_time": 0,
-                "accuracy": accuracy_score(y_true=y_test, y_pred=y_test_pred),
-                "f1_score": f1_score(
-                    y_true=y_test, y_pred=y_test_pred, average="macro"
-                ),
-                "precision": precision_score(
-                    y_true=y_test, y_pred=y_test_pred, average="macro"
-                ),
-                "recall": recall_score(
-                    y_true=y_test, y_pred=y_test_pred, average="macro"
-                ),
-                "confusion_matrix": confusion_matrix(
-                    y_true=y_test,
-                    y_pred=y_test_pred,
-                    labels=list(self.label_map.keys()),
-                ).tolist(),
-            },
-            {
-                "model": model_name,
-                "task": task_name,
-                "split": "validation",
-                "evaluation_time": 0,
-                "accuracy": accuracy_score(y_true=y_val, y_pred=y_val_pred),
-                "f1_score": f1_score(y_true=y_val, y_pred=y_val_pred, average="macro"),
-                "precision": precision_score(
-                    y_true=y_val, y_pred=y_val_pred, average="macro"
-                ),
-                "recall": recall_score(
-                    y_true=y_val, y_pred=y_val_pred, average="macro"
-                ),
-                "confusion_matrix": confusion_matrix(
-                    y_true=y_val,
-                    y_pred=y_val_pred,
-                    labels=list(self.label_map.keys()),
-                ).tolist(),
-            },
-            {
-                "model": model_name,
-                "task": task_name,
-                "split": "training",
-                "evaluation_time": 0,
-                "accuracy": accuracy_score(y_true=y_train, y_pred=y_train_pred),
-                "f1_score": f1_score(
-                    y_true=y_train, y_pred=y_train_pred, average="macro"
-                ),
-                "precision": precision_score(
-                    y_true=y_train, y_pred=y_train_pred, average="macro"
-                ),
-                "recall": recall_score(
-                    y_true=y_train, y_pred=y_train_pred, average="macro"
-                ),
-                "confusion_matrix": confusion_matrix(
-                    y_true=y_train,
-                    y_pred=y_train_pred,
-                    labels=list(self.label_map.keys()),
-                ).tolist(),
-            },
-        ]
+        if not only_test:
+            y_train_pred = model.predict(x_train)
+            y_val_pred = model.predict(x_val)
+
+            all_evaluations = [
+                {
+                    "model": model_name,
+                    "task": task_name,
+                    "split": "test",
+                    "evaluation_time": 0,
+                    "accuracy": accuracy_score(y_true=y_test, y_pred=y_test_pred),
+                    "f1_score": f1_score(
+                        y_true=y_test, y_pred=y_test_pred, average="macro"
+                    ),
+                    "precision": precision_score(
+                        y_true=y_test, y_pred=y_test_pred, average="macro"
+                    ),
+                    "recall": recall_score(
+                        y_true=y_test, y_pred=y_test_pred, average="macro"
+                    ),
+                    "confusion_matrix": confusion_matrix(
+                        y_true=y_test,
+                        y_pred=y_test_pred,
+                        labels=list(self.label_map.keys()),
+                    ).tolist(),
+                },
+                {
+                    "model": model_name,
+                    "task": task_name,
+                    "split": "validation",
+                    "evaluation_time": 0,
+                    "accuracy": accuracy_score(y_true=y_val, y_pred=y_val_pred),
+                    "f1_score": f1_score(
+                        y_true=y_val, y_pred=y_val_pred, average="macro"
+                    ),
+                    "precision": precision_score(
+                        y_true=y_val, y_pred=y_val_pred, average="macro"
+                    ),
+                    "recall": recall_score(
+                        y_true=y_val, y_pred=y_val_pred, average="macro"
+                    ),
+                    "confusion_matrix": confusion_matrix(
+                        y_true=y_val,
+                        y_pred=y_val_pred,
+                        labels=list(self.label_map.keys()),
+                    ).tolist(),
+                },
+                {
+                    "model": model_name,
+                    "task": task_name,
+                    "split": "training",
+                    "evaluation_time": 0,
+                    "accuracy": accuracy_score(y_true=y_train, y_pred=y_train_pred),
+                    "f1_score": f1_score(
+                        y_true=y_train, y_pred=y_train_pred, average="macro"
+                    ),
+                    "precision": precision_score(
+                        y_true=y_train, y_pred=y_train_pred, average="macro"
+                    ),
+                    "recall": recall_score(
+                        y_true=y_train, y_pred=y_train_pred, average="macro"
+                    ),
+                    "confusion_matrix": confusion_matrix(
+                        y_true=y_train,
+                        y_pred=y_train_pred,
+                        labels=list(self.label_map.keys()),
+                    ).tolist(),
+                },
+            ]
 
         # Write the evaluation to a JSON file
         json.dump(
@@ -202,5 +207,8 @@ class Evaluator:
                 encoding="utf-8",
             ),
         )
+
+        if only_test:
+            return test_evaluation
 
         return all_evaluations
